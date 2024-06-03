b5f68ae282be415f9981e36806868c09a823ca04
boltx123 <udaygarg8188@gmail.com>
2024-06-03 23:39:00 +0530

    update

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -19,38 +19,37 @@ const AddProduct = () => {
         setProductDetails({...productDetails,[e.target.name]:e.target.value})
     }
 
-    const Add_Product = async ()=>{
+    const Add_Product = async () => {
         console.log(productDetails);
         let responseData;
         let product = productDetails;
-
+      
         let formData = new FormData();
-        formData.append('product',image);
-
-        await fetch(`${process.env.REACT_APP_BACKURL}/upload`,{
-            method:'POST',
-            headers:{
-                Accept:'application/json',
+        formData.append('product', image);
+      
+        await fetch(`${process.env.REACT_APP_BACKURL}/upload`, {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+          },
+          body: formData,
+        }).then((resp) => resp.json()).then((data) => { responseData = data; });
+      
+        if (responseData.success) {
+          product.image = responseData.image_url;
+          console.log(product);
+          await fetch(`${process.env.REACT_APP_BACKURL}/addproduct`, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
             },
-            body:formData,
-        }).then((resp) => resp.json()).then((data)=>{responseData=data});
-
-        if(responseData.success)
-            {
-                product.image=responseData.image_url;
-                console.log(product);
-                await fetch(`${process.env.REACT_APP_BACKURL}/addproduct`,{
-                    method:'POST',
-                    headers:{
-                        Accept:'application/json',
-                        'Content-Type':'application/json',
-                    },
-                    body:JSON.stringify(product),
-                }).then((resp)=>resp.json()).then((data)=>{
-                        data.success?alert("Product Added"):alert("Failed")
-                })
-            }
-    }
+            body: JSON.stringify(product),
+          }).then((resp) => resp.json()).then((data) => {
+            data.success ? alert("Product Added") : alert("Failed")
+          })
+        }
+      }
 
   return (
     <div className='add-product'>
