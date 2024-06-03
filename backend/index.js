add113b9967faec1fbda8300fcd4636026bbca38
boltx123 <udaygarg8188@gmail.com>
2024-06-03 23:47:33 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -61,7 +61,7 @@ app.post("/upload", upload.single('product'), async (req, res) => {
   try {
     const response = await axios.post('https://api.imgur.com/3/image', req.file.buffer, {
       headers: {
-        'Authorization': `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+        'Authorization': `${process.env.IMGUR_CLIENT_ID}`,
         'Content-Type': req.file.mimetype
       }
     });
