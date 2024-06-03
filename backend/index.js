96b990c12f2b8be4e4ddbb294acb204d4e0ba5ed
boltx123 <udaygarg8188@gmail.com>
2024-06-03 23:34:22 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const axios = require('axios');
+
 
 app.use(express.json());
 app.use(cors());
